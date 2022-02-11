e763ae400ee2102afe1db0c3b674da7cbf3a096c
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-02-11 15:11:03 -0600

    Added models

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:covcopcomp_math_fact/screens/wrapper.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,16 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:covcopcomp_math_fact/models/usermodel.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // anon
+  // Create a custom user object from response
+  UserModel? _userFromFirebaseUser(User? user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
+
+  // Sign into app anonymously
   Future signInAnonymous() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
-
       User? user = result.user;
-
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
 
