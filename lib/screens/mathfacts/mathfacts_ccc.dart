ae20097714b3a6c814e64f8c37cdc7cfd3178995
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-02-13 16:33:06 -0600

    Beginning of dynamic answer area

--- lib/screens/mathfacts/mathfacts_ccc.dart
+++ lib/screens/mathfacts/mathfacts_ccc.dart
@@ -8,8 +8,68 @@ class MathFactsCCC extends StatefulWidget {
 }
 
 class _MathFactsCCCState extends State<MathFactsCCC> {
+  List<Object> probList = [
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    ["3", "+", "2", "=", "5"],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+  ];
+
+  Column _createView(List<Object> probList) {
+    int rows = probList.length;
+
+    List<Widget> dynContainer = [];
+    List<Widget> dynRows = [];
+
+    probList.forEach((element) {
+      List<Object> mod = element as List<Object>;
+      print(mod);
+
+      int len = mod.length;
+
+      List<Widget> dynRow = [];
+
+      for (int c = 0; c < len; c++) {
+        String value = mod[c] ?? "";
+
+        Container dynC = Container(
+          margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+          decoration: BoxDecoration(color: Colors.white, border: Border.all()),
+          height: 50,
+          child: Text(value),
+        );
+
+        Expanded dyn = Expanded(
+          child: dynC,
+        );
+
+        dynRow.add(dyn);
+      }
+
+      dynRows.add(Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: dynRow,
+      ));
+    });
+
+    //dynContainer.add(newRow);
+    //print(dynRow);
+
+    //Row newRow = Row(
+    //  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //  children: dynRow,
+    //);
+
+    return Column(
+      children: dynRows,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mWidget = _createView(probList);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cover, Copy, Compare'),
@@ -19,6 +79,7 @@ class _MathFactsCCCState extends State<MathFactsCCC> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              /*
               Expanded(
                 child: Container(
                   margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
@@ -27,6 +88,10 @@ class _MathFactsCCCState extends State<MathFactsCCC> {
                   height: double.infinity,
                   child: const Text(""),
                 ),
+              ),
+              */
+              Expanded(
+                child: mWidget,
               ),
               Expanded(
                 child: Column(children: [
@@ -170,7 +235,7 @@ class _MathFactsCCCState extends State<MathFactsCCC> {
                         style: TextButton.styleFrom(
                             backgroundColor: Colors.green,
                             primary: Colors.white),
-                        child: const Text("<-", textAlign: TextAlign.center),
+                        child: const Text("Del", textAlign: TextAlign.center),
                       )),
                       const SizedBox(width: 5),
                       Expanded(
@@ -188,7 +253,7 @@ class _MathFactsCCCState extends State<MathFactsCCC> {
                         style: TextButton.styleFrom(
                             backgroundColor: Colors.green,
                             primary: Colors.white),
-                        child: const Text("->", textAlign: TextAlign.center),
+                        child: const Text("=", textAlign: TextAlign.center),
                       )),
                       const SizedBox(width: 5),
                       Expanded(
