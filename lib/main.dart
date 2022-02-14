fd03b2556fa783a7404fbbde86c4ab0f95da72b7
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-02-14 15:59:45 -0600

    Start of licensing docs

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,46 @@
 // @dart=2.9
+
+/* 
+    The MIT License
+    Copyright February 1, 2022 Shawn Gilroy/Louisiana State University
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
 import 'package:covcopcomp_math_fact/models/usermodel.dart';
 import 'package:covcopcomp_math_fact/screens/authrouting.dart';
 import 'package:covcopcomp_math_fact/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+
+  SystemChrome.setEnabledSystemUIOverlays([]).then((_) {
+    SystemChrome.setPreferredOrientations(
+            [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft])
+        .then((_) {
+      runApp(const MyApp());
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
