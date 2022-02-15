da6a2a47fd319f8cb0841d998ecbceae5c6f470c
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-02-14 21:58:11 -0600

    Add lic's

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 /* 
     The MIT License
     Copyright February 1, 2022 Shawn Gilroy/Louisiana State University
+    
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,3 +1,26 @@
+/* 
+    The MIT License
+    Copyright February 1, 2022 Shawn Gilroy/Louisiana State University
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:covcopcomp_math_fact/models/record_ccc_mfacts.dart';
 import 'package:covcopcomp_math_fact/models/usermodel.dart';
@@ -19,8 +42,6 @@ class DatabaseService {
 // ------------------------
 // Modern Calls here
 // ------------------------
-
-// Maps
 
   List<Student> _studentListingFromSnapshot(QuerySnapshot snapshot) {
     return snapshot.docs.map((d) {
@@ -47,8 +68,6 @@ class DatabaseService {
         currentSchool: data['school']);
   }
 
-// Streams
-
   Stream<UserData> get userData {
     return mainCollection.doc(uid).snapshots().map(_userDataFromSnapshot);
   }
@@ -59,8 +78,6 @@ class DatabaseService {
         .snapshots()
         .map(_studentListingFromSnapshot);
   }
-
-// Updates
 
   // Add Student to classroom collection
   Future addToStudentCollection(
