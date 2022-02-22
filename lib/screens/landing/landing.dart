8dfcbbec08814fa603c22bcbd7bcbbb358fe5f78
Shawn Gilroy <shawnpgilroy@gmail.com>
2022-02-22 17:42:51 -0600

    Amend image

--- lib/screens/landing/landing.dart
+++ lib/screens/landing/landing.dart
@@ -2,8 +2,6 @@ import 'package:covcopcomp_math_fact/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import '../authrouting.dart';
-
 class LandingScreen extends StatefulWidget {
   const LandingScreen({Key key}) : super(key: key);
 
@@ -21,7 +19,7 @@ class _LandingScreenState extends State<LandingScreen> {
 
     final double iconWidth = screen.width * 0.20;
 
-    final Widget svg1 = SvgPicture.asset('assets/Anonymous_Paper_1_icon.svg',
+    final Widget svg1 = SvgPicture.asset('assets/Anonymous_Paper_2_icon.svg',
         width: screen.width * 0.20, height: screen.height * 0.20);
 
     final Widget svg2 = SvgPicture.asset(
@@ -39,11 +37,11 @@ class _LandingScreenState extends State<LandingScreen> {
           left: centerX - iconWidth / 2.0,
           top: centerY / 2.0,
         ),
-        Positioned(
-          child: svg1,
-          left: centerX - iconWidth / 2.0 - iconWidth * 0.5,
-          top: centerY / 2.0 + iconWidth * 0.1,
-        ),
+        //Positioned(
+        //  child: svg1,
+        //  left: centerX - iconWidth / 2.0 - iconWidth * 0.5,
+        //  top: centerY / 2.0 + iconWidth * 0.1,
+        //),
         const Positioned.fill(
           child: Center(
               child: Text(
@@ -60,6 +58,7 @@ class _LandingScreenState extends State<LandingScreen> {
         Positioned.fill(
           child: Center(
               child: TextButton(
+                  //style: ButtonStyle(backgroundColor: ),
                   child: const Text(
                     "Launch",
                     style: cccTextStyle,
